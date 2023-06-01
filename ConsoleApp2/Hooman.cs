19361a573a557b009b9465da5d9996d6019871e2
hvu26510 <hvu26510@gmail.com>
2023-06-01 19:17:31 +0700

    Add project files.

---
+++ ConsoleApp2/Hooman.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Hooman
+    {
+        protected string Name;
+        protected string Address;
+        protected string Email;
+        protected string PhoneNumber;
+
+        public Hooman(string name, string address, string email, string phoneNumber)
+        {
+            Name = name;
+            Address = address;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+
+        public virtual void Show()
+        {
+            Console.WriteLine("Ten: " + this.Name);
+            Console.WriteLine("Dia Chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+        }
+    }
+
+    class NhanVien : Hooman
+    {
+        protected string BangCap;
+
+        public NhanVien(string name, string address, string email, string phoneNumber, string BangCap) : 
+            base(name, address, email, phoneNumber)
+        {
+            this.BangCap = BangCap;
+        }
+
+
+        public override void Show()
+        {
+            Console.WriteLine("Ten: " + this.Name);
+            Console.WriteLine("Dia Chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+            Console.WriteLine("Bang Cap: " + this.BangCap);
+           
+
+        }
+
+    }
+
+    class KeToan : NhanVien
+    {
+        private int Luong;
+
+        public KeToan(string name, string address, string email, string phoneNumber, string BangCap, int Luong) : 
+            base(name, address, email, phoneNumber, BangCap)
+        {
+            this.Luong = Luong;
+        }
+
+        public override void Show()
+        {
+            Console.WriteLine("Ten: " + this.Name);
+            Console.WriteLine("Dia Chi: " + this.Address);
+            Console.WriteLine("Email: " + this.Email);
+            Console.WriteLine("SDT: " + this.PhoneNumber);
+            Console.WriteLine("Bang Cap: " + this.BangCap);
+            Console.WriteLine("Luong: " + this.Luong);
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //NhanVien nv1 = new NhanVien("Nguyen van A", "Ha Noi", "anv@gmail.com", "0123456789", "Phu Ho - Cap Cao");
+
+            //nv1.Show();
+
+            KeToan kt1 = new KeToan("Nguyen van A", "Ha Noi", "anv@gmail.com", "0123456789", "Phu Ho - Cap Cao", 15000000);
+
+            kt1.Show();
+
+            Console.ReadLine();
+        }
+    }
+}

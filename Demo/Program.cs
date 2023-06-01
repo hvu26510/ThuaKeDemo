810376b3c9e7186912a79f917b3c3fbe6f5346df
hvu26510 <hvu26510@gmail.com>
2023-06-01 20:12:56 +0700

    push

---
+++ Demo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList listSV = new ArrayList();
+
+            // tạo đối tượng sv1 kiểu SinhVien
+            SinhVien sv1 = new SinhVien();
+            sv1.Nhap();
+
+            //thêm đối tượng vào listSV
+            listSV.Add(sv1);
+
+            // tạo đối tượng sv2 kiểu SinhVien
+            SinhVien sv2 = new SinhVien();
+            sv2.Nhap();
+
+            //thêm đối tượng vào listSV
+            listSV.Add(sv2);
+
+
+
+            Console.Clear();
+
+            //xoa sv1 khoi listSV
+
+            listSV.Remove(sv1);
+
+            foreach (SinhVien i in listSV)
+            {
+                i.Show();
+            }
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ Demo/SinhVien.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class SinhVien
+    {
+        private string name;
+        private double diem;
+        private string email;
+
+        public string HocLuc()
+        {
+            if(this.diem < 5)
+            {
+                return "Yeu";
+            }else if (this.diem < 6.5)
+            {
+                return "TB";
+            }else if (this.diem < 7.5)
+            {
+                return "Kha";
+            }else if(this.diem < 9)
+            {
+                return "gioi";
+            }else
+            {
+                return "Xuat sac";
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Ten:"+ this.name);
+            Console.WriteLine("Diem:" + this.diem);
+            Console.WriteLine("email:" + this.email);
+            Console.WriteLine("Hoc luc:" + this.HocLuc());
+        }
+
+        public void Nhap()
+        {
+            Console.Write("Ten:" );
+            this.name = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Diem:");
+            this.diem = double.Parse( Console.ReadLine() );
+            Console.WriteLine();
+            Console.Write("email:" + this.email);
+            this.email = Console.ReadLine();
+        }
+
+
+    }
+}
